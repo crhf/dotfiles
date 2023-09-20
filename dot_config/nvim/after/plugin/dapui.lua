af404962b490ccd89642174258797c144b85205d
crhf <ruanhf8@gmail.com>
2023-09-20 17:49:03 +0800

    nvim: change keymap

--- dot_config/nvim/after/plugin/dapui.lua
+++ dot_config/nvim/after/plugin/dapui.lua
@@ -2,7 +2,7 @@ local dap, dapui = require("dap"), require("dapui")
 require("dapui").setup()
 
 vim.keymap.set('n', "<leader>ui", require('dapui').toggle)
-vim.keymap.set("n", "<leader>4", function() require('dapui').float_element(nil, {enter=true}) end, {desc="dapui float"})
+-- vim.keymap.set("n", "<leader>4", function() require('dapui').float_element(nil, {enter=true}) end, {desc="dapui float"})
 vim.keymap.set("n", "<leader>de", function() require('dapui').eval(nil, {context="repl", enter=true}) end, {desc="dapui eval"})
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
