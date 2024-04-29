0ebc288f986af3d51a6680e632329ee5df96cddc
crhf <ruanhf8@gmail.com>
2024-04-29 12:43:25 +0800

    nvim: add copilot

--- dot_config/nvim/lua/crhf/packer.lua
+++ dot_config/nvim/lua/crhf/packer.lua
@@ -587,6 +587,59 @@ return require("packer").startup(function(use)
 			vim.keymap.set({ "n", "v" }, "<leader>af", "<cmd>ALEFix<CR>", {desc = "ALEFix"})
 		end,
 	})
+
+    use {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require('copilot').setup({
+                panel = {
+                    enabled = false,
+                    auto_refresh = true,
+                    keymap = {
+                        jump_prev = "[[",
+                        jump_next = "]]",
+                        accept = "<CR>",
+                        refresh = "gr",
+                        open = "<M-CR>"
+                    },
+                    layout = {
+                        position = "bottom", -- | top | left | right
+                        ratio = 0.4
+                    },
+                },
+                suggestion = {
+                    enabled = true,
+                    auto_trigger = true,
+                    debounce = 75,
+                    keymap = {
+                        accept = "<C-f>",
+                        accept_word = false,
+                        accept_line = false,
+                        next = "<C-s>",
+                        prev = "<C-t>",
+                        dismiss = false,
+                    },
+                },
+                filetypes = {
+                    yaml = false,
+                    markdown = false,
+                    help = false,
+                    gitcommit = false,
+                    gitrebase = false,
+                    hgcommit = false,
+                    svn = false,
+                    cvs = false,
+                    ["."] = false,
+                },
+                copilot_node_command = 'node', -- Node.js version must be > 18.x
+                server_opts_overrides = {},
+            })
+        end,
+    }
+    --
+    -- use("github/copilot.vim")
 end)
 
 -- cffooze
