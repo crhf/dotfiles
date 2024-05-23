3e0685939902a76beb17bb357fa1a8f3623045cf
crhf <ruanhf8@gmail.com>
2024-05-23 11:10:30 +0800

    nvim: update foldlevel

--- dot_config/nvim/after/plugin/nvim_ufo.lua
+++ dot_config/nvim/after/plugin/nvim_ufo.lua
@@ -1,6 +1,6 @@
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 vim.o.foldcolumn = '3' -- '0' is not bad
-vim.o.foldlevel = 1 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
