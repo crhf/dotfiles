6591d14071802a3b5f839332504275bf4536337e
crhf <hruan@comp.nus.edu.sg>
2025-01-21 22:07:18 +0800

    add gnome extensions

--- dot_config/setup.sh
+++ dot_config/setup.sh
@@ -9,6 +9,7 @@ chezmoi init --apply https://github.com/crhf/dotfiles.git
 
 # neovim
 nix-env -iA nixpkgs.neovim
+sudo apt-get install -y ripgrep
 
 # zoxide
 nix-env -iA nixpkgs.zoxide
@@ -32,3 +33,30 @@ n lts
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux source ~/.tmux.conf
+
+# python3-venv, for ruff etc.
+sudo apt-get install -y python3-venv
+
+# brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# lazygit
+nix-env -iA nixpkgs.lazygit
+
+# lazydocker
+brew install jesseduffield/lazydocker/lazydocker
+
+# gnome extensions
+  # workspace-switcher-manager@G-dH.github.com
+  # space-bar@luchrioh
+  # user-theme@gnome-shell-extensions.gcampax.github.com
+  # stopwatch@aliakseiz.github.com
+  # cosmic-dock@system76.com
+  # cosmic-workspaces@system76.com
+  # ding@rastersoft.com
+  # pop-cosmic@system76.com
+  # pop-shell@system76.com
+  # popx11gestures@system76.com
+  # system76-power@system76.com
+  # ubuntu-appindicators@ubuntu.com
+
