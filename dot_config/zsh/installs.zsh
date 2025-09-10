ab8610523d23c12666da015a1257db3d061f2cf3
crhf <hruan@comp.nus.edu.sg>
2025-09-10 12:25:44 +0800

    remove pyenv

--- dot_config/zsh/installs.zsh
+++ dot_config/zsh/installs.zsh
@@ -67,15 +67,15 @@ alias zj='zellij'
 # ~/.bash_profile if it exists, otherwise ~/.profile (for login shells)
 # and ~/.bashrc (for interactive shells) :
 
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+# export PYENV_ROOT="$HOME/.pyenv"
+# [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+# eval "$(pyenv init -)"
 
 # Restart your shell for the changes to take effect.
 
 # Load pyenv-virtualenv automatically by adding
 # the following to ~/.bashrc:
 
-eval "$(pyenv virtualenv-init -)"
+# eval "$(pyenv virtualenv-init -)"
 
 alias lzd=lazydocker
