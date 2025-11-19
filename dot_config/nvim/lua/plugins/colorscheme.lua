ab7668cce9bfe1104b9209033fcca8ab0099e64b
crhf <hruan@comp.nus.edu.sg>
2025-11-19 20:01:19 +0800

    update nvim

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -54,12 +54,6 @@ return {
     "metalelf0/black-metal-theme-neovim",
     lazy = false,
     priority = 1000,
-    config = function()
-      require("black-metal").setup({
-        -- optional configuration here
-      })
-      require("black-metal").load()
-    end,
   },
   {
     "LazyVim/LazyVim",
