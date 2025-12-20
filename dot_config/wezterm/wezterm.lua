029050c9e218d1c7bb22e4dbf55cac077b119b7d
Haifeng Ruan <haifeng@m2.local>
2025-12-20 17:18:52 +0800

    update wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,6 +1,25 @@
 local config = {}
 
+local wezterm = require("wezterm")
+
 config.color_scheme = "Gruvbox Dark (Gogh)"
 config.font_size = 20
+
+config.keys = {
+	-- ... your other key bindings ...
+
+	{
+		key = "T",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+}
 
 return config
